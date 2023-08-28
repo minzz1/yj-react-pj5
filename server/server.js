0105aac6971b0f6e1034dc2f3bc58bc1b414f8eb
minzz1 <hohohohohoh52@gmail.com>
2023-08-28 09:50:23 +0900

    0828

--- server/server.js
+++ server/server.js
@@ -16,6 +16,7 @@ app.use(express.json());
 
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
 
+// 서버로 오는 모든 루트를 index.html 파일로 보낸다.
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "/build/index.html"));
 });
